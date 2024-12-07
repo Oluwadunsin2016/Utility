4305205888ba68e2953aab357143d64b69c2c8a0
Oluwadunsin <oluwadunsin2016@gmail.com>
2024-12-07 21:55:40 +0100

    Updated

--- vite.config.js
+++ vite.config.js
@@ -4,35 +4,35 @@ import react from "@vitejs/plugin-react";
 
 // console.log("PostCSS Config:", postcss);
 
-export default defineConfig({
-  plugins: [react()],
-  // css: {
-  //   postcss,
-  // },
-  build: {
-    lib: {
-      entry: "./src/utility.jsx",
-      name: "UtilityWidget",
-      fileName: (format) => `utility-widget.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"], // Keep React as external
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-      onwarn(warning, warn) {
-        if (warning.plugin === "vite:css") {
-          console.warn("PostCSS Warning:", warning.message);
-        }
-        warn(warning);
-      },
-    },
-  },
-});
-
 // export default defineConfig({
 //   plugins: [react()],
+//   css: {
+//     postcss,
+//   },
+//   build: {
+//     lib: {
+//       entry: "./src/utility.jsx",
+//       name: "UtilityWidget",
+//       fileName: (format) => `utility-widget.${format}.js`,
+//     },
+//     rollupOptions: {
+//       external: ["react", "react-dom"], // Keep React as external
+//       output: {
+//         globals: {
+//           react: "React",
+//           "react-dom": "ReactDOM",
+//         },
+//       },
+//       onwarn(warning, warn) {
+//         if (warning.plugin === "vite:css") {
+//           console.warn("PostCSS Warning:", warning.message);
+//         }
+//         warn(warning);
+//       },
+//     },
+//   },
 // });
+
+export default defineConfig({
+  plugins: [react()],
+});
