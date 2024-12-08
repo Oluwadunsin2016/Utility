a8ebbc753f47482841aefb23cc8525c3693e0ce9
Oluwadunsin <oluwadunsin2016@gmail.com>
2024-12-08 23:47:09 +0100

    Updated

--- utility.js
+++ utility.js
@@ -12,7 +12,7 @@
 //   widgetContainer.appendChild(iframe);
 // })();
 
-window.UtilityWidget = function () {
+window.CcUtilityWidget = function () {
   return {
     iframe: null,
     baseurl: "https://utility-virid.vercel.app",
